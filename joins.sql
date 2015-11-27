c86aa1efc46f36898d516b6c95125c1ea9f6ffe1
AlexBennett <alexanderbondbennett@gmail.com>
2015-11-26 22:07:35 -0500

    changed join file

--- joins.sql
+++ joins.sql
@@ -16,3 +16,5 @@ FROM lost_dog_collars LEFT JOIN dog_owners ON lost_dog_collars.dog_name = dog_ow
 #4 Display all dog owners we know about; if collar is in our possession, display that also
 SELECT dog_owners.id AS owner_id, dog_owners.name AS owner_name, lost_dog_collars.dog_name
 FROM dog_owners LEFT JOIN lost_dog_collars ON lost_dog_collars.dog_name = dog_owners.dog_name;
+
+SELECT
